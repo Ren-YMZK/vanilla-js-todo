02cc9bb557e309e925a90ad597af8b40e12a0777
Ren Yamazaki <r26763041@gmail.com>
2025-06-26 00:14:40 +0900

    タスクの追加機能(テキスト)

--- src/main.js
+++ src/main.js
@@ -1,9 +1,36 @@
-import './style.css'
+import "./style.css";
 
+const onClickAdd = () => {
+  //テキストボックスの値を取得
+  const inputText = document.getElementById("add-text").value;
 
+  //テキストボックスの値を初期化
+  document.getElementById("add-text").value = "";
 
-document.querySelector('#app').innerHTML = `
+  //liを生成
+  const li = document.createElement("li");
+
+  //divを生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //pを生成
+  const p = document.createElement("p");
+  p.className = "todo-item";
+  p.innerText = inputText;
+
+  //階層構造を実現
+  div.appendChild(p);
+  li.appendChild(div);
+
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);
+
+document.querySelector("#app").innerHTML = `
   <div>
     
   </div>
-`
+`;
