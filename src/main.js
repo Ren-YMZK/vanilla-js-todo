35bc7eef849f32720ec8f0267b51b2237e6a8318
Ren Yamazaki <r26763041@gmail.com>
2025-06-26 00:46:20 +0900

    タスクの追加機能(ボタン)

--- src/main.js
+++ src/main.js
@@ -19,8 +19,36 @@ const onClickAdd = () => {
   p.className = "todo-item";
   p.innerText = inputText;
 
+  //button(完了)を生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    //押された完了ボタンの親にあるli配下の完了ボタンと削除ボタンを削除
+    const moveTarget = completeButton.closest("li");
+    completeButton.nextElementSibling.remove();
+    completeButton.remove();
+    //戻すボタンを生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    moveTarget.firstElementChild.appendChild(backButton);
+
+    //完了リストに移動
+    document.getElementById("complete-list").appendChild(moveTarget);
+  });
+
+  //button(削除)を生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //押された削除ボタンにあるliを未完了リストから削除
+    const deleteTarget = deleteButton.closest("li");
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
   //階層構造を実現
   div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
   li.appendChild(div);
 
   //未完了リストに追加
